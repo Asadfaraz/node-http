c5bd5cba9b0b2084a9015d94f68e1cfb4fce8618
Asad Faraz <asadfaraz44@gmail.com>
2021-04-17 15:10:57 +0500

    request url

--- learning-http.js
+++ learning-http.js
@@ -3,13 +3,35 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   // providing headers in reqeust
   //   res.writeHead(200, { "content-type": "text/plain" });
-  res.writeHead(200, { "content-type": "text/html" });
-  // prividing body
-  res.write("<h1>Home Page </h1>");
-  // ending the request
-  res.end();
+  // res.writeHead(200, { "content-type": "text/html" });
+  // // prividing body
+  // res.write("<h1>Home Page </h1>");
+  // // ending the request
+  // res.end();
 
+  // Working with url
+  const url = req.url;
+  console.log(url);
   console.log("User hit the server");
+
+  // Home Page
+  if (url == "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1> Home Page </h1>");
+    res.end();
+  }
+  // about page
+  else if (url == "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page </h1>");
+    res.end();
+  }
+  // error page
+  else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Page not Found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
